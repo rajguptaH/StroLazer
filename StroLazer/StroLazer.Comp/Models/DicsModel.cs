3e13c10a9b58473b251b02a107af3aa58e692344
rajguptaH <rajkumar00999.rk@gmail.com>
2023-01-21 08:38:32 +0530

    Created StroLazer Project

---
+++ StroLazer/StroLazer.Comp/Models/DicsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StroLazer.Comp.Models
+{
+    public class DicsModel
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ StroLazer/StroLazer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace StroLazer;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
