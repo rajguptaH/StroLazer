d62a27647326399b3ba301a7003bb62117514ec3
rajguptaH <rajkumar00999.rk@gmail.com>
2023-02-04 11:51:29 +0530

    included WebApi

---
+++ WebApiExample/WebApiExample/Utility/Connection/DbConnectionBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using WebApiCrud.Library.Connection.Interface;
+
+namespace WebApiCrud.Library.Connection
+{
+
+    public class ConnectionBuilder : IConnectionBuilder 
+    { 
+
+        public ConnectionBuilder()
+        {
+          
+        }
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                {
+                  
+                        var connectionString = "Server=localhost;Database=Maui; User ID=sa;Password=admin;";
+                    try
+                    {
+                        SqlConnection connection = new SqlConnection(connectionString);
+                        return connection;
+                    }
+                    catch (Exception ex)
+                    {
+                        return null;
+                    }
+                    
+                    
+                    
+                }
+            }
+        }
+    }
+}
